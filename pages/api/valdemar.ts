5f809a002d07f99f5eca7b6a54fa613b9cf392dd
ANNE LUND <ANNELS87@HOTMAIL.com>
2022-11-02 10:32:29 +0100

    ...

--- pages/api/valdemar.ts
+++ pages/api/valdemar.ts
@@ -1,7 +1,13 @@
 import executeQuery from "../../src/db";
 import { NextApiRequest, NextApiResponse } from "next";
+import NextCors from 'nextjs-cors'
 
 export default async function getAllWishesById(req: NextApiRequest, res: NextApiResponse) {
+    await NextCors(req, res, {
+        methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+        origin: '*',
+        optionsSuccesStatus: 200,
+    })
    
 if(req.method === "GET") {
    
