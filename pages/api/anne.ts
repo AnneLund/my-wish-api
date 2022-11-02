e8a5d3380386a9aa3251d0caa604f573ece9793f
ANNE LUND <ANNELS87@HOTMAIL.com>
2022-11-02 10:54:18 +0100

    ...

--- pages/api/anne.ts
+++ pages/api/anne.ts
@@ -24,4 +24,49 @@ else{
     res.status(500).json({message: "Route not valid"})
 }
 
+if(req.method === "PUT") {
+    const {titel, image, id, url, description, købt} = req.body
+    console.log(req.body)
+    if(!id){
+        res.status(422).json({message: "Invalid id!"})
+        return;
+    }
+
+    if(købt && id && !titel && !description && !url && !image) {
+        await executeQuery(`UPDATE anne SET købt = '${købt}' WHERE id = '${id}'`)
+        res.status(201).json({message: "'Købt' opdateret!"})
+        return;
+            }  
+
+      else if (titel && description && url && image && id) {
+   await executeQuery(`UPDATE anne SET titel = '${titel}', image = '${image}', url = '${url}', description = '${description}' WHERE id = '${id}'`)
+    res.status(201).json({message: "Data updated!"})
+    return;
+    }          
+    }
+
+if(req.method === "DELETE") {
+    const {id} = req.body
+    if(!id){
+        res.status(422).json({message: "Invalid data"})
+        return;
+    }
+await executeQuery(`DELETE FROM anne WHERE id = '${id}'`)
+res.status(201).json({message: "Data deleted!"})
+return;
+}
+
+if(req.method === "POST") {
+    const {titel, description, image, url, købt} = req.body
+
+    // if(!username || !password){
+    //     res.status(422).json({message: "Invalid data"})
+    //     return;
+    // }
+
+    const data = await executeQuery(`INSERT INTO anne(titel, description, image, url, købt) VALUES('${titel}', '${description}', '${image}', '${url}', '${købt}')`)
+    res.status(201).json({message: "Data created!", data})
+    return;
+}
+
 }
--- pages/api/mikkel.ts
+++ pages/api/mikkel.ts
@@ -24,4 +24,48 @@ else{
     res.status(500).json({message: "Route not valid"})
 }
 
+if(req.method === "PUT") {
+    const {titel, image, id, url, description, købt} = req.body
+    console.log(req.body)
+    if(!id){
+        res.status(422).json({message: "Invalid id!"})
+        return;
+    }
+
+    if(købt && id && !titel && !description && !url && !image) {
+        await executeQuery(`UPDATE mikkel SET købt = '${købt}' WHERE id = '${id}'`)
+        res.status(201).json({message: "'Købt' opdateret!"})
+        return;
+            }  
+
+      else if (titel && description && url && image && id) {
+   await executeQuery(`UPDATE mikkel SET titel = '${titel}', image = '${image}', url = '${url}', description = '${description}' WHERE id = '${id}'`)
+    res.status(201).json({message: "Data updated!"})
+    return;
+    }          
+    }
+
+if(req.method === "DELETE") {
+    const {id} = req.body
+    if(!id){
+        res.status(422).json({message: "Invalid data"})
+        return;
+    }
+await executeQuery(`DELETE FROM mikkel WHERE id = '${id}'`)
+res.status(201).json({message: "Data deleted!"})
+return;
+}
+
+if(req.method === "POST") {
+    const {titel, description, image, url, købt} = req.body
+
+    // if(!username || !password){
+    //     res.status(422).json({message: "Invalid data"})
+    //     return;
+    // }
+
+    const data = await executeQuery(`INSERT INTO mikkel(titel, description, image, url, købt) VALUES('${titel}', '${description}', '${image}', '${url}', '${købt}')`)
+    res.status(201).json({message: "Data created!", data})
+    return;
+}
 }
