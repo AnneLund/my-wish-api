c3c9d40cd0818941084c9411d7ec6aa756a4a318
ANNE LUND <ANNELS87@HOTMAIL.com>
2022-11-02 10:11:39 +0100

    done

---
+++ pages/api/anne/[id].ts
@@ -0,0 +1,17 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import executeQuery from "../../../src/db";
+
+export default async function  getAllWishById(req: NextApiRequest, res: NextApiResponse) {
+
+    if(req.method === "GET") {
+        const data = await executeQuery(`SELECT * FROM anne WHERE id='${req.query.id}'`)
+        res.status(201).json({data})
+        console.log(data)
+        return;
+        }
+        
+        else{
+            res.status(500).json({message: "Route not valid"})
+        }
+        
+}
---
+++ pages/api/rebecca.ts
@@ -0,0 +1,20 @@
+import executeQuery from "../../src/db";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function getAllWishesById(req: NextApiRequest, res: NextApiResponse) {
+   
+if(req.method === "GET") {
+   
+const data = await executeQuery(`SELECT * FROM wishes`)
+
+res.status(201).json({message: "Data fetch", data})
+
+console.log(data)
+return;
+}
+
+else{
+    res.status(500).json({message: "Route not valid"})
+}
+
+}
