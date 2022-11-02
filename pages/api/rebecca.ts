5b612f82442cb5d5e64d10dd939a8ef5839d3fbe
ANNE LUND <ANNELS87@HOTMAIL.com>
2022-11-02 13:45:56 +0100

    users

--- pages/api/rebecca.ts
+++ pages/api/rebecca.ts
@@ -23,4 +23,51 @@ else{
     res.status(500).json({message: "Route not valid"})
 }
 
+if(req.method === "PUT") {
+    const {titel, image, id, url, description, købt} = req.body
+    console.log(req.body)
+    if(!id){
+        res.status(422).json({message: "Invalid id!"})
+        return;
+    }
+
+    if(købt && id && !titel && !description && !url && !image) {
+        await executeQuery(`UPDATE wishes SET købt = '${købt}' WHERE id = '${id}'`)
+        res.status(201).json({message: "'Købt' opdateret!"})
+        return;
+            }  
+
+      else if (titel && description && url && image && id) {
+   await executeQuery(`UPDATE wishes SET titel = '${titel}', image = '${image}', url = '${url}', description = '${description}' WHERE id = '${id}'`)
+    res.status(201).json({message: "Data updated!"})
+    return;
+    }          
+    }
+
+if(req.method === "DELETE") {
+    const {id} = req.body
+    if(!id){
+        res.status(422).json({message: "Invalid data"})
+        return;
+    }
+await executeQuery(`DELETE FROM wishes WHERE id = '${id}'`)
+res.status(201).json({message: "Data deleted!"})
+return;
+}
+
+
+if(req.method === "POST") {
+    const {titel, description, image, url, købt} = req.body
+
+    // if(!username || !password){
+    //     res.status(422).json({message: "Invalid data"})
+    //     return;
+    // }
+
+    const data = await executeQuery(`INSERT INTO wishes(titel, description, image, url, købt) VALUES('${titel}', '${description}', '${image}', '${url}', '${købt}')`)
+    res.status(201).json({message: "Data created!", data})
+    return;
+}
+
+
 }
---
+++ pages/api/users.ts
@@ -0,0 +1,59 @@
+import executeQuery from "../../src/db";
+import NextCors from 'nextjs-cors'
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function getAllUsersById(req: NextApiRequest, res: NextApiResponse) {
+
+    await NextCors(req, res, {
+        methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+        origin: '*',
+        optionsSuccesStatus: 200,
+    })
+
+    if(req.method === "PUT") {
+        const {username, password} = req.body
+        if(!username || !password){
+            res.status(422).json({message: "Invalid data"})
+            return;
+        }
+await executeQuery(`UPDATE users SET username = '${username}' WHERE username = '${username}'`)
+res.status(201).json({message: "Data updated!"})
+return;
+    }
+
+    if(req.method === "DELETE") {
+        const {id} = req.body
+        if(!id){
+            res.status(422).json({message: "Invalid data"})
+            return;
+        }
+await executeQuery(`DELETE FROM users WHERE id = '${id}'`)
+res.status(201).json({message: "Data deleted!"})
+return;
+    }
+
+    if(req.method === "POST") {
+        const {username, password} = req.body
+
+        if(!username || !password){
+            res.status(422).json({message: "Invalid data"})
+            return;
+        }
+
+        const user = await executeQuery(`INSERT INTO users(username, password) VALUES('${username}', '${password}')`)
+        res.status(201).json({message: "Data created!", user})
+        return;
+    }
+
+
+if(req.method === "GET") {
+
+const user = await executeQuery(`SELECT * FROM users`)
+res.status(201).json({message: "Data fetch", user})
+
+return;
+}
+else{
+    res.status(500).json({message: "Route not valid"})
+}
+}
