68b34c78d22c14416356f5b6b19ad579483da1f3
ANNE LUND <ANNELS87@HOTMAIL.com>
2022-11-02 18:48:33 +0100

    ...

--- pages/api/anne/[id].ts
+++ pages/api/anne/[id].ts
@@ -15,7 +15,7 @@ export default async function  getAllWishById(req: NextApiRequest, res: NextApiR
     if(req.method === "GET") {
      
         const data = await executeQuery(`SELECT * FROM anne WHERE id = ${req.query.id}`)
-        res.status(201).json({message: `Fetching ${req.query.id}`, data})
+        res.status(201).json({message: `Fetching id: ${req.query.id}`, data})
         console.log(data)
         return;
         }
@@ -23,5 +23,26 @@ export default async function  getAllWishById(req: NextApiRequest, res: NextApiR
         else{
             res.status(500).json({message: "Route not valid"})
         }
+
+        if(req.method === "PUT") {
+            const {titel, image, id, url, description, købt} = req.body
+            console.log(req.body)
+            if(!id){
+                res.status(422).json({message: "Invalid id!"})
+                return;
+            }
+        
+            if(købt && id && !titel && !description && !url && !image) {
+                await executeQuery(`UPDATE anne SET købt = '${købt}' WHERE id = '${id}'`)
+                res.status(201).json({message: "'Købt' opdateret!"})
+                return;
+                    }  
+        
+              else if (titel && description && url && image && id) {
+           await executeQuery(`UPDATE anne SET titel = '${titel}', image = '${image}', url = '${url}', description = '${description}' WHERE id = '${id}'`)
+            res.status(201).json({message: "Data updated!"})
+            return;
+            }          
+            }
         
 }
